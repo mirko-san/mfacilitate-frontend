2fde2894682be09ed7afbb9d418e98d7a901448a
mirko-san <41379069+mirko-san@users.noreply.github.com>
2023-04-09 15:08:30 +0900

    :art: Add link to AppBar

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -1,9 +1,18 @@
 <template>
   <v-app-bar flat>
     <v-app-bar-title>
-      <v-icon icon="mdi-circle-slice-4" />
-
-      Base Preset
+      <span class="mr-2">
+        <v-icon icon="mdi-link" />
+        <router-link to="/">home</router-link>
+      </span>
+      <span class="mr-2">
+        <v-icon icon="mdi-link" />
+        <router-link to="/meetings">meetings</router-link>
+      </span>
+      <span class="mr-2">
+        <v-icon icon="mdi-link" />
+        <router-link to="/themes">themes</router-link>
+      </span>
     </v-app-bar-title>
   </v-app-bar>
 </template>
